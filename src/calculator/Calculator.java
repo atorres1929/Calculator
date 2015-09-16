6186c5b6acc96b903312df03a50413f94442206d
Adam Torres <atorres1929@gmail.com>
2015-09-16 19:28:14 -0400

    Added Power Logic and started Menu Bar options

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -17,6 +17,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -32,7 +34,7 @@ public class Calculator extends JFrame{
     public final Dimension prefferedSize = new Dimension(FRAME_HEIGHT,FRAME_WIDTH);
     private JTextField text;
     private JTextField answerField;
-    private String defaultText = "(6+2-8*16*4)/100-8";
+    private String defaultText = "2^2^2";
     ArrayList<JButton> numbers;
     JButton bPoint,bEnter,bMult,bDiv,bSub,bAdd,bBack,bC,bCE;
     
@@ -41,13 +43,19 @@ public class Calculator extends JFrame{
         addButtonFunction();
     }
     private boolean isNumber(String param){
-        return (getIndexOfNextOperator(param) == -1 && getIndexOfPreviousOperator(param) == -1) 
-                || (param.startsWith("-") && getIndexOfNextOperator(param) == -1);
+        try{
+            Double.valueOf(param);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
     private String calculate(String param){
-        if (isNumber(param)){
-            System.out.println("\n");
+        if (isNumber(param) || (isNumber(param) && numOperands(param) == 1)){
             return param;
+        }
+        else if (param.contains("^")){
+            return parseModule(param, "^");
         }
         else if (param.contains("(")){
             int previous = param.indexOf("(");
@@ -73,54 +81,83 @@ public class Calculator extends JFrame{
     Ex: parseModule("5+7-18*2/4*2/2", "*") returns 
     
     */
-    private String parseModule(String module, String operator){
-        System.out.println("Module to parse: "+module);
-        System.out.println("Operator used: "+operator);
-        
-        BigDecimal part1,part2;
-        int index = module.indexOf(operator);
+    private String parseModule(String module, String operator) {
+        BigDecimal ans = BigDecimal.ZERO;
+        try{
+            System.out.println("Module to parse: "+module);
+            System.out.println("Operator used: "+operator);
+            
+            int index;
+            if ("^".equals(operator)){
+                index = module.indexOf(operator, module.lastIndexOf(operator));
+            }
+            else{
+                index = module.indexOf(operator);
+            }
+            
             if (index == 0)
                 index = module.substring(1).indexOf(operator);
-        int previous = getIndexOfPreviousOperator(module.substring(0, index));
-        int next = getIndexOfNextOperator(module.substring(index+1));
-        System.out.println(module.substring(previous+1, index));System.out.println(previous+1+" "+index);
-        part1 = new BigDecimal(module.substring(previous+1, index));
-        if (numOperators(module) == 2 && previous == '-')
-            part2 = new BigDecimal('-'+module.substring(index+2));
-        else if (next == -1)
-            part2 = new BigDecimal(module.substring((index+1),module.length()));
-        else
-            part2 = new BigDecimal(module.substring(index+1, index+next+1));
-        
-        System.out.println("part1: "+part1+" || part2: "+part2);
-        BigDecimal ans;
-        switch(operator){
-            case "*":
-                ans = part1.multiply(part2);
-                break;
-            case "/":
-                ans = part1.divide(part2);
-                break;
-            case "+":
-                ans = part1.add(part2);
-                break;
-            case "-":
-                    ans = part1.subtract(part2);
-                break;
-            default:
-                ans = new BigDecimal(0);
+            int previous = getIndexOfPreviousOperator(module.substring(0, index));
+            int next = getIndexOfNextOperator(module.substring(index+1));
+            BigDecimal part1,part2;
+
+            if(module.startsWith("-")){
+                part1 = new BigDecimal(module.substring(previous, index+1));
+            }
+            else if (operator.equals("^")){
+                part1 = new BigDecimal(module.substring(previous+1, module.lastIndexOf(operator)));
+            }
+            else{
+                part1 = new BigDecimal(module.substring(previous+1, index));
+            }
+            System.out.print("part1: "+part1);
+            if (operator == "^")
+                part2 = new BigDecimal(module.substring(index+1, module.length()));
+            else if (next == -1)
+                part2 = new BigDecimal(module.substring((index+1),module.length()));
+            else
+                part2 = new BigDecimal(module.substring(index+1, index+next+1));
+
+            System.out.println(" || part2: "+part2);
+
+
+            switch(operator){
+                case "^":
+                    ans = part1.pow(part2.intValue());
+                    break;
+                case "*":
+                    ans = part1.multiply(part2);
+                    break;
+                case "/":
+                    ans = part1.divide(part2);
+                    break;
+                case "+":
+                    ans = part1.add(part2);
+                    break;
+                case "-":
+                    if (module.startsWith("-"))
+                        ans = part1.add(part2);
+                    else
+                        ans = part1.subtract(part2);
+                    break;
+                default:
+                    ans = new BigDecimal(0);
+            }
+            
+            System.out.println("Answer: "+ans);
+            if (next == -1 && (previous == -1 || ans.toString().startsWith("-")))
+                return calculate(String.valueOf(ans));
+            else if (next == -1 && previous != -1)
+                return calculate(module.substring(0, previous+1)+String.valueOf(ans));
+            else if (next != -1 && previous == -1)
+                return calculate(String.valueOf(ans)+module.substring(index+next+1));
+            else if (next != -1 && previous != -1)
+                return calculate(module.substring(0, previous+1)+String.valueOf(ans)+module.substring(index+next+1));
+
+            System.out.println("Error when parsing module: "+module);
+        }catch(Exception e){
+            e.printStackTrace();
         }
-        System.out.println("Answer: "+ans);
-        if (next == -1 && previous == -1)
-            return calculate(String.valueOf(ans));
-        else if (next == -1 && previous != -1)
-            return calculate(module.substring(0, previous+1)+String.valueOf(ans));
-        else if (next != -1 && previous == -1)
-            return calculate(String.valueOf(ans)+module.substring(index+next+1));
-        else if (next != -1 && previous != -1)
-            return calculate(module.substring(0, previous+1)+String.valueOf(ans)+module.substring(index+next+1));
-        
-        System.out.println("Error when parsing module: "+module);
         return "Error";
     }
     /*Gets the index of the first operator in the specified string.
@@ -128,7 +165,7 @@ public class Calculator extends JFrame{
     */
     private int getIndexOfPreviousOperator(String param){
         for (int i = param.length()-1; i >= 0; i--){
-            if (param.charAt(i) == '/' || param.charAt(i) == '*' || param.charAt(i) == '-' || param.charAt(i) == '+'){
+            if (param.charAt(i) == '/' || param.charAt(i) == '*' || param.charAt(i) == '-' || param.charAt(i) == '+' || param.charAt(i) == '^'){
 //                System.out.println("IndexOfPreviousOperator: " + i);
                 return i;
             }    
@@ -149,6 +186,14 @@ public class Calculator extends JFrame{
     }
     
     //Gets number of operators for special cases
+    private int numOperands(String module){
+        int numOperands = 0;
+        for(int i = 0; i < module.length(); i++){
+            if (module.charAt(i) == '+' || module.charAt(i) == '-' || module.charAt(i) == '/' || module.charAt(i) == '*')
+                numOperands++;
+        }
+        return numOperands;
+    }
     private int numOperators(String module){
         int numOps = 0;
         for (int i = 0; i < module.length(); i++){
@@ -305,12 +350,24 @@ public class Calculator extends JFrame{
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         
-        text = new JTextField("");
+        String[] menuNames = {"Mode", "Tools"};
+        JMenuBar menuBar = new JMenuBar();
         
+        ArrayList<JMenu> menus = new ArrayList<>();
+        for (int i = 0; i < menuNames.length; i++){
+            JMenu jmenu;
+            menus.add(new JMenu(menuNames[i]));
+            menuBar.add(menus.get(i));
+        }
+        menuBar.setEnabled(true);
+        menuBar.setVisible(true);
+        
+        setJMenuBar(menuBar);
+        
+        text = new JTextField("");
         text.setText(defaultText);
         text.setFont(new Font("Arial", Font.PLAIN, 40));
-        c.gridx = 0;
-        c.gridy = 0;
+        c.gridy = 1;
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1;
         c.weighty = 1;
@@ -350,7 +407,7 @@ public class Calculator extends JFrame{
         buttons1.add(numbers.get(1));
         buttons1.add(bSub);
         
-        c.gridy = 1;
+        c.gridy = 2;
         c.weighty = 8;
         add(buttons1,c);
         JPanel buttons2 = new JPanel(new GridLayout(0,2));
@@ -360,7 +417,7 @@ public class Calculator extends JFrame{
         buttons3.add(bEnter);
         buttons2.add(buttons3);
         
-        c.gridy = 2;
+        c.gridy = 3;
         c.weighty = 2;
         add(buttons2,c);
         
@@ -368,7 +425,7 @@ public class Calculator extends JFrame{
         answerField = new JTextField("");
         answerField.setFont(new Font("Arial", Font.BOLD, 40));
         answer.add(answerField);
-        c.gridy = 3;
+        c.gridy = 4;
         c.weighty = 8;
         
         add(answer, c);
